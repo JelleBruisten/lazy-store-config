d2caefd92c51250ccf3a76d44c0244d3fe74d78b
Jelle Bruisten <jelle_bruisten@hotmail.com>
2021-06-26 15:51:47 +0200

    initial commit

---
+++ apps/lazy-store-config/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lazy-store-config-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'lazy-store-config';
+}
